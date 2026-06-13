93fb7f2eb6f2f7e41614ffc0386417ac9ee4e92c
rbhaju <rohanbhaju@gmail.com>
2026-06-13 19:57:07 +1000

    Kepto site

---
+++ js/reveal.js
@@ -0,0 +1,15 @@
+/* Reveals elements with .reveal as they scroll into view. Shared by all pages. */
+(function () {
+  var io = new IntersectionObserver(function (entries) {
+    entries.forEach(function (e) {
+      if (e.isIntersecting) {
+        e.target.classList.add('in');
+        io.unobserve(e.target);
+      }
+    });
+  }, { threshold: 0.25, rootMargin: '0px 0px -40px 0px' });
+
+  document.querySelectorAll('.reveal').forEach(function (el) {
+    io.observe(el);
+  });
+})();
